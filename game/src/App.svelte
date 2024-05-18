520422d813555089e14f97756d744a3dcf8d82dc
ClassicMC <classicmclive@gmail.com>
2024-05-18 14:46:39 +0000

    frfr

--- game/src/App.svelte
+++ game/src/App.svelte
@@ -1,10 +1,18 @@
 <script>
   import { fade } from 'svelte/transition';
+  import Txt from './lib/txt.svelte'
+  import Roll from './lib/roll.svelte'
   let visable = false;
   function startUp(){visable = true;}
 </script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
-
+  :global(body){
+    background-image: url(./lib/skibidi.jpg);
+    background-repeat: none;
+    background-size:cover;
+    background-attachment: fixed;
+  }
 </style>
 <svelte:window
   on:load={startUp()}
@@ -15,9 +23,14 @@
       <span class="h1" in:fade={{delay: 100 + i * 150, duration: 800}}>{letter}</span>
     {/if}
   {/each}<br/>
-  {#each 'In axit avim des orem' as letter,i}
-     {#if visable}
-      <span class="em" in:fade={{delay: 2000 + i * 50, duration: 0}}>{letter}</span>
-     {/if}
-  {/each}
+  <Txt txt='In axit orem des livi'/><br/><br/><br/><br/>
+  
+  {#if visable}
+    <div in:fade={{delay: 2510 + 2 * 150, duration: 800}}>    
+      <Roll/>
+    </div>
+  {/if}
 </main>
+<maintwoelectricbogaloo>
+  skibidi
+</maintwoelectricbogaloo>
