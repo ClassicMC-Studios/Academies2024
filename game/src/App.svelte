497cc0de06186ee9cd75d8fa56e6ec318a2a4e64
ClassicMC <classicmclive@gmail.com>
2024-05-18 15:12:16 +0000

    slay

--- game/src/App.svelte
+++ game/src/App.svelte
@@ -8,10 +8,12 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
   :global(body){
-    background-image: url(./lib/skibidi.jpg);
+    background-image: url(./lib/scock3.jpg);
     background-repeat: none;
     background-size:cover;
+    background-color:#fff;
     background-attachment: fixed;
+    color:white;
   }
 </style>
 <svelte:window
@@ -32,5 +34,5 @@
   {/if}
 </main>
 <maintwoelectricbogaloo>
-  skibidi
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde non, illum minima sit cupiditate doloribus architecto enim doloremque vero distinctio similique vitae itaque magnam facilis quod error adipisci, accusamus quos! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore, corporis officia laboriosam ipsa minus eligendi, quod provident distinctio, odit consequuntur deleniti voluptate magnam impedit aut animi odio? Aut, reiciendis expedita? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus cumque nobis numquam iusto mollitia sed dolorum, id ratione perferendis assumenda pariatur rerum sint corporis architecto incidunt laboriosam. Earum, provident corporis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia consectetur recusandae voluptas veritatis natus numquam itaque, ipsa ad accusamus rerum commodi explicabo! Itaque similique incidunt vitae, commodi nam dolorem nostrum. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum debitis hic odio at provident ea corrupti excepturi eveniet aspernatur. Incidunt laborum pariatur doloribus beatae sunt, quia id rerum perferendis molestiae? Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, doloribus perspiciatis quod laudantium ipsa cum numquam provident dolorem nam maiores illo. Veritatis impedit, veniam molestias necessitatibus tenetur cumque! Culpa, quidem.<br/><br/>
 </maintwoelectricbogaloo>
--- game/src/lib/roll.svelte
+++ game/src/lib/roll.svelte
@@ -22,6 +22,7 @@
     position:absolute;
     left:50%;
     transform: translate(-50%);
+		font-weight:bolder;
   }
 </style>
 
